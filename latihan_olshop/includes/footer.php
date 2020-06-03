94a1696bd9a9cebd2bdf906da222ec5cfafa0508
Agung Tri A <agung.t.atmojo@gmail.com>
2020-06-03 20:56:53 +0700

    fix: fix misplaced footer

--- latihan_olshop/includes/footer.php
+++ latihan_olshop/includes/footer.php
@@ -16,6 +16,13 @@
   <script src="vendor/jquery/jquery.min.js"></script>
   <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
   <script src="vendor/fancybox/jquery.fancybox.min.js"></script>
+  <script>
+    $(document).ready(function() {
+      if ($(document).height() <= 657) {
+        $("footer").addClass("fixed-bottom");
+      }
+    });
+  </script>
 </body>
 
 </html>
